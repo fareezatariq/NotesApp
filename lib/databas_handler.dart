b46b1dc302f5d1159b687a53660c3cd3dfd83446
fareezatariq <fareezatariq@gmail.com>
2023-03-01 23:00:43 +0500

    added db onCreate func

---
+++ lib/databas_handler.dart
@@ -0,0 +1,27 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:notes_app/notes.dart';
+import 'dart:io' as io;
+import 'package:path/path.dart';
+class DBHelper{
+  static Database? _db ;
+  Future<Database?> get db async{
+    if(_db !=null){
+      return _db;
+    }
+    _db=await initDatabase();
+    return _db;
+  }
+  initDatabase()async{
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path= join(documentDirectory.path, 'notes.db');
+    var db= await openDatabase(path, version: 1,onCreate: _onCreate);
+    return db;
+  }
+  _onCreate(Database db, int version) async{
+    await db.execute(
+      'CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, age INTEGER NOT NULL, description TEXT NOT NULL, email TEXT)'
+    );
+  }
+
+}
